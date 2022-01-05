19acbce02bcc6848837958babc42023f5ef0b961
algosketch <sjy_9484@naver.com>
2022-01-05 22:10:25 +0900

    add routeIds

--- app/src/main/java/org/algosketch/inubus/global/usecase/WriteMemoUseCase.kt
+++
@@ -1,9 +0,0 @@
-package org.algosketch.inubus.global.usecase
-
-import org.algosketch.inubus.data.repository.Repository
-
-class WriteMemoUseCase(private val repository: Repository) {
-//    suspend fun run(content: String) {
-//        repository.writeMemo(content)
-//    }
-}
