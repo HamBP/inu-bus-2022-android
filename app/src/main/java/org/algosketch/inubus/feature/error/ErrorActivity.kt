af492d75da7079c2400dfa1b2494193da8594011
algosketch <sjy_9484@naver.com>
2022-01-08 17:04:24 +0900

    add : 시간제한

---
+++ app/src/main/java/org/algosketch/inubus/feature/error/ErrorActivity.kt
@@ -0,0 +1,12 @@
+package org.algosketch.inubus.feature.error
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.algosketch.inubus.R
+
+class ErrorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_error)
+    }
+}
--- app/src/main/java/org/algosketch/inubus/feature/splash/SplashActivity.kt
+++ app/src/main/java/org/algosketch/inubus/feature/splash/SplashActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import org.algosketch.inubus.R
+import org.algosketch.inubus.feature.error.ErrorActivity
 import org.algosketch.inubus.feature.main.MainActivity
+import java.time.LocalDateTime
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +16,14 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            val dateTime = LocalDateTime.now()
+            if(dateTime.hour in 7..22) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                val intent = Intent(this, ErrorActivity::class.java)
+                startActivity(intent)
+            }
             finish()
         }, 500)
     }
