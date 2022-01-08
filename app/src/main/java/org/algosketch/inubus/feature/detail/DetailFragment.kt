b5fe5165fdba8461609fee05cdc94d293160b848
algosketch <sjy_9484@naver.com>
2022-01-09 07:37:00 +0900

    add : change background color

--- app/src/main/java/org/algosketch/inubus/feature/detail/DetailFragment.kt
+++ app/src/main/java/org/algosketch/inubus/feature/detail/DetailFragment.kt
@@ -1,13 +1,15 @@
 package org.algosketch.inubus.feature.detail
 
-import android.util.Log
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.MutableLiveData
 import androidx.navigation.fragment.findNavController
 import org.algosketch.inubus.R
 import org.algosketch.inubus.databinding.FragmentDetailBinding
 import org.algosketch.inubus.global.base.BaseFragment
-import org.algosketch.inubus.global.store.Store
 
 class DetailFragment : BaseFragment<FragmentDetailBinding>() {
     override val layoutResourceId = R.layout.fragment_detail
@@ -25,5 +27,26 @@ class DetailFragment : BaseFragment<FragmentDetailBinding>() {
         viewModel.busNumber.postValue(arguments?.getString("busNumber"))
         viewModel.exit.postValue("정류장은 ${arguments?.getString("where")}역 ${arguments?.getInt("exit")}번 출구에서")
         viewModel.distance.postValue("${arguments?.getInt("distance")}m")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val colorMap = mapOf(
+            "red" to R.color.red_bus,
+            "blue" to R.color.blue_bus,
+            "green" to R.color.green_bus,
+            "purple" to R.color.purple_bus
+        )
+
+        val toolbarBackground = view.findViewById<View>(R.id.toolbar_background)
+        setBackgroundTint(toolbarBackground, colorMap[arguments?.getString("busColor")] ?: R.color.black_3)
+    }
+
+    fun setBackgroundTint(view: View, colorId: Int) {
+        var drawable: Drawable? = DrawableCompat.wrap(view.background!!)
+        drawable = DrawableCompat.wrap(drawable!!)
+
+        DrawableCompat.setTint(drawable, ContextCompat.getColor(view.context, colorId))
     }
 }
