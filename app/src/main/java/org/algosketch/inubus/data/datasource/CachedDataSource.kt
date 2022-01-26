e84bbe3137e5b546b8d4d7f04f824cf1bf65738e
algosketch <sjy_9484@naver.com>
2022-01-26 23:39:20 +0900

    refactor : 캐시된 데이터를 DataSource 에서 가져오는 것으로 변경

--- app/src/main/java/org/algosketch/inubus/data/datasource/CachedDataSource.kt
+++ app/src/main/java/org/algosketch/inubus/data/datasource/CachedDataSource.kt
@@ -5,8 +5,16 @@ import org.algosketch.inubus.data.model.BusArrival
 import org.algosketch.inubus.global.store.Store
 
 class CachedDataSource : DataSource {
+    val updatedAt: MutableMap<String, String> = HashMap()
+    private val cachedBusArrival: MutableMap<String, BusArrival> = HashMap()
+
     override suspend fun getArrivalBusTime(bstopId: String): BusArrival {
-        Log.d("캐시 요청", bstopId)
-        return Store.cachedBusArrival[bstopId] ?: BusArrival(null, null, null)
+        Log.d("$bstopId 에 대한 캐시 요청", "메모리에 저장된 데이터를 반환합니다.")
+        return cachedBusArrival[bstopId] ?: BusArrival(null, null, null)
+    }
+
+    fun storeData(bstopId: String, busArrival: BusArrival, dateString: String) {
+        cachedBusArrival[bstopId] = busArrival
+        updatedAt[bstopId] = dateString
     }
 }
