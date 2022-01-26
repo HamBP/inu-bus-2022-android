0fea6047e6f06f737f6c6de5c3420caab5c3766b
algosketch <sjy_9484@naver.com>
2022-01-26 23:39:20 +0900

    refactor : 캐시된 데이터를 DataSource 에서 가져오는 것으로 변경

--- app/src/main/java/org/algosketch/inubus/data/repository/BusArrivalRepositoryImpl.kt
+++ app/src/main/java/org/algosketch/inubus/data/repository/BusArrivalRepositoryImpl.kt
@@ -2,10 +2,8 @@ package org.algosketch.inubus.data.repository
 
 import android.util.Log
 import org.algosketch.inubus.data.datasource.CachedDataSource
-import org.algosketch.inubus.data.datasource.DataSource
 import org.algosketch.inubus.data.datasource.RemoteDataSource
 import org.algosketch.inubus.data.model.BusArrival
-import org.algosketch.inubus.global.store.Store
 import org.algosketch.inubus.global.usecase.BusArrivalRepository
 import java.time.LocalDateTime
 
@@ -13,22 +11,18 @@ class BusArrivalRepositoryImpl(
     private val cachedDataSource: CachedDataSource,
     private val remoteDataSource: RemoteDataSource
 ) : BusArrivalRepository {
-    private val updatedAt: MutableMap<String, String> = HashMap()
-    private val cachedBusArrival: MutableMap<String, BusArrival> = HashMap()
-
     override suspend fun getArrivalBusTime(bstopId: String) : BusArrival {
         val currentTime = LocalDateTime.now()
         val dateString = "${currentTime.dayOfMonth}:${currentTime.hour}:${currentTime.minute}"
 
         val result: BusArrival?
-        if(dateString == updatedAt[bstopId]){
-            result = cachedBusArrival[bstopId]!!
-            Log.d("bstopId - $bstopId", "캐시된 데이터를 반환합니다.")
+        if(dateString == cachedDataSource.updatedAt[bstopId]){
+            Log.e("t", "cached")
+            result = cachedDataSource.getArrivalBusTime(bstopId)
         }
         else {
             result = remoteDataSource.getArrivalBusTime(bstopId)
-            cachedBusArrival[bstopId] = result
-            updatedAt[bstopId] = dateString
+            cachedDataSource.storeData(bstopId, result, dateString)
         }
         return result
     }
