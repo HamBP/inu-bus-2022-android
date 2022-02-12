50bce1a75664ead94d399929061f3078e3f069b0
algosketch <sjy_9484@naver.com>
2022-02-13 01:28:00 +0900

    refactor : MutableLiveData<String> -> String 으로 변경

--- app/src/main/java/org/algosketch/inubus/domain/entity/BusArrival.kt
+++ app/src/main/java/org/algosketch/inubus/domain/entity/BusArrival.kt
@@ -1,13 +1,11 @@
 package org.algosketch.inubus.domain.entity
 
-import androidx.lifecycle.MutableLiveData
-
 data class BusArrival(
     val restTime: Int,
     val busNumber: String,
     val busColor: String,
     val exit: Int,
     val where: String,
-    val restTimeInformationText: MutableLiveData<String>,
-    val exitInformationText: MutableLiveData<String>
+    val restTimeInformationText: String,
+    val exitInformationText: String
 )
