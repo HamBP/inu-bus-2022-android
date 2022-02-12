52a54773d9a19cf9d085f8150003f2a76ab10d64
algosketch <sjy_9484@naver.com>
2022-02-13 00:10:05 +0900

    refactor : extract bind function

--- app/src/main/java/org/algosketch/inubus/ui/home/BusListAdapter.kt
+++ app/src/main/java/org/algosketch/inubus/ui/home/BusListAdapter.kt
@@ -13,83 +13,82 @@ import androidx.core.graphics.drawable.DrawableCompat
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.core.os.bundleOf
+import org.algosketch.inubus.databinding.ItemInformationBinding
+import org.algosketch.inubus.domain.entity.BusArrival
 import org.algosketch.inubus.global.util.Bus
 
 
-class BusListAdapter(val list: List<org.algosketch.inubus.domain.entity.BusArrival>) : RecyclerView.Adapter<BusListAdapter.BusViewHolder>() {
+class BusListAdapter(val list: List<BusArrival>) : RecyclerView.Adapter<BusListAdapter.BusViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BusViewHolder {
         return BusViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: BusViewHolder, position: Int) {
-        val where = list[position].where
-        val exit = list[position].exit
-        val restTime = list[position].restTime
-        val busNumber = list[position].busNumber
-        val distance = Bus.getDistance(where, busNumber)
-        val busColor = Bus.getBusColorByBusNumber(busNumber)
-
-        setBusNumber(holder.busNumber, busNumber)
-
-        holder.exit.text = "${where}역 ${exit}번 출구"
-        holder.busArrivalTime.text = "버스가 ${restTime}분 뒤 도착해요."
-        holder.view.setOnClickListener {
-            val navController = holder.view.findNavController()
-            val bundle = getBundle(exit, where, busNumber, distance, restTime, busColor)
-            navController.navigate(R.id.action_wrap_to_detail, bundle)
-        }
-        holder.tagRecyclerView.adapter = TagAdapter(Bus.getBusStopsByBusNumber(busNumber))
-    }
-
-    fun getBundle(exit: Int, where: String, busNumber: String, distance: Int, restTime: Int, busColor: String) : Bundle {
-        val bundle = bundleOf(
-            "exit" to exit,
-            "where" to where,
-            "busNumber" to busNumber,
-            "distance" to distance,
-            "restTime" to restTime,
-            "busColor" to busColor
-        )
-
-        return bundle
+        holder.bind(list[position])
     }
 
     override fun getItemCount() = list.size
 
-    class BusViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    class BusViewHolder private constructor(val view: View, val binding: ItemInformationBinding) : RecyclerView.ViewHolder(view) {
+        fun bind(item: BusArrival) {
+            binding.item = item
+
+            setBusNumber(binding.busNumber, item.busNumber)
+
+            val where = item.where
+            val exit = item.exit
+            val restTime = item.restTime
+            val distance = Bus.getDistance(where, item.busNumber)
+            val busColor = Bus.getBusColorByBusNumber(item.busNumber)
+
+            binding.exit.text = "${where}역 ${exit}번 출구"
+            binding.busArrivalTime.text = "버스가 ${restTime}분 뒤 도착해요."
+            binding.root.setOnClickListener {
+                val navController = it.findNavController()
+                val bundle = getBundle(exit, where, item.busNumber, distance, restTime, busColor)
+                navController.navigate(R.id.action_wrap_to_detail, bundle)
+            }
+            binding.tagRecyclerView.adapter = TagAdapter(Bus.getBusStopsByBusNumber(item.busNumber))
+        }
+
+        fun getBundle(exit: Int, where: String, busNumber: String, distance: Int, restTime: Int, busColor: String) : Bundle {
+            val bundle = bundleOf(
+                "exit" to exit,
+                "where" to where,
+                "busNumber" to busNumber,
+                "distance" to distance,
+                "restTime" to restTime,
+                "busColor" to busColor
+            )
+
+            return bundle
+        }
+
         companion object {
             fun from(parent: ViewGroup) : BusViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.item_information, parent, false)
+                val binding = ItemInformationBinding.inflate(layoutInflater, parent, false)
 
-                return BusViewHolder(view)
+                return BusViewHolder(binding.root, binding)
             }
         }
+        fun setBusNumber(view: TextView, busNumber: String) {
+            val busColor = Bus.getBusColorByBusNumber(busNumber)
+            val colorMap = mapOf(
+                "red" to R.color.red_bus,
+                "blue" to R.color.blue_bus,
+                "green" to R.color.green_bus,
+                "purple" to R.color.purple_bus
+            )
 
-        val busNumber: TextView = view.findViewById(R.id.bus_number)
-        val exit: TextView = view.findViewById(R.id.exit)
-        val busArrivalTime: TextView = view.findViewById(R.id.bus_arrival_time)
-        val tagRecyclerView: RecyclerView = view.findViewById(R.id.tag_recycler_view)
-    }
+            setBackgroundTint(view, colorMap[busColor] ?: R.color.black_3)
+        }
 
-    fun setBusNumber(view: TextView, busNumber: String) {
-        view.text = busNumber
+        fun setBackgroundTint(view: View, colorId: Int) {
+            var drawable: Drawable? = view.background
+            drawable = DrawableCompat.wrap(drawable!!)
 
-        val busColor = Bus.getBusColorByBusNumber(busNumber)
-        val colorMap = mapOf(
-            "red" to R.color.red_bus,
-            "blue" to R.color.blue_bus,
-            "green" to R.color.green_bus,
-            "purple" to R.color.purple_bus
-        )
-
-        setBackgroundTint(view, colorMap[busColor] ?: R.color.black_3)
-    }
-
-    fun setBackgroundTint(view: View, colorId: Int) {
-        var drawable: Drawable? = view.background
-        drawable = DrawableCompat.wrap(drawable!!)
-
-        DrawableCompat.setTint(drawable, ContextCompat.getColor(view.context, colorId))
+            DrawableCompat.setTint(drawable, ContextCompat.getColor(view.context, colorId))
+        }
     }
 }
