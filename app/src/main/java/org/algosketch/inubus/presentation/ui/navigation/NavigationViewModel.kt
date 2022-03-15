49dee4bb3acf603a6108094c74959303739b0405
algosketch <sjy_9484@naver.com>
2022-03-16 08:24:27 +0900

    refactor : 기존 코틀린 코드 xml 로 이동

--- app/src/main/java/org/algosketch/inubus/presentation/ui/navigation/NavigationViewModel.kt
+++ app/src/main/java/org/algosketch/inubus/presentation/ui/navigation/NavigationViewModel.kt
@@ -2,7 +2,17 @@ package org.algosketch.inubus.presentation.ui.navigation
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.material.tabs.TabLayout
 
 class NavigationViewModel : ViewModel() {
     val currentTab = MutableLiveData(0)
+
+    val onTabSelectedListener = object : TabLayout.OnTabSelectedListener {
+        override fun onTabSelected(tab: TabLayout.Tab?) {
+            currentTab.value = tab?.position
+        }
+
+        override fun onTabUnselected(tab: TabLayout.Tab?) {}
+        override fun onTabReselected(tab: TabLayout.Tab?) {}
+    }
 }
