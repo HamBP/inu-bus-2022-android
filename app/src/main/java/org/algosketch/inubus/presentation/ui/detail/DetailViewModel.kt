48401cc9db27b6773fb0eb78c71936ca996c40c4
algosketch <sjy_9484@naver.com>
2022-05-30 00:52:09 +0900

    fix : upgrade gradle version & fix DetailFragment

--- app/src/main/java/org/algosketch/inubus/presentation/ui/detail/DetailViewModel.kt
+++ app/src/main/java/org/algosketch/inubus/presentation/ui/detail/DetailViewModel.kt
@@ -2,11 +2,14 @@ package org.algosketch.inubus.presentation.ui.detail
 
 import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import org.algosketch.inubus.common.base.BaseViewModel
 import org.algosketch.inubus.common.util.SingleLiveEvent
 
 class DetailViewModel : BaseViewModel() {
     val busNumber = MutableStateFlow("")
+//    val busNumber = _busNumber.asStateFlow()
+
     val restTime = MutableStateFlow("")
     val exit = MutableStateFlow("")
     val distance = MutableStateFlow("")
