92931e6963f16f279fbd4f1a1201f9d829153a37
algosketch <sjy_9484@naver.com>
2023-02-11 23:50:01 +0900

    feat : [홈] 드롭다운 버튼

--- app/src/main/java/org/algosketch/inubus/presentation/ui/inu/InuScreen.kt
+++ app/src/main/java/org/algosketch/inubus/presentation/ui/inu/InuScreen.kt
@@ -2,24 +2,30 @@ package org.algosketch.inubus.presentation.ui.inu
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.flow.collectLatest
+import org.algosketch.inubus.R
 import org.algosketch.inubus.common.util.Bus
 import org.algosketch.inubus.domain.entity.BusArrivalInfo
 import org.algosketch.inubus.presentation.ui.home.HomeViewModel
@@ -31,6 +37,12 @@ fun InuScreen(viewModel: HomeViewModel, owner: LifecycleOwner, subwayState: Stri
     val updatedTime = remember {
         mutableStateOf(viewModel.currentTime.value)
     }
+    val filter = remember {
+        mutableStateOf(viewModel.filter.value)
+    }
+    val onFilterItemClicked = { filterItem: String ->
+        viewModel.filter.value = filterItem
+    }
 
     viewModel.busList.observe(owner) {
         busList.value = viewModel.busList.value ?: listOf()
@@ -40,6 +52,12 @@ fun InuScreen(viewModel: HomeViewModel, owner: LifecycleOwner, subwayState: Stri
             updatedTime.value = it
         }
     }
+    owner.lifecycleScope.launchWhenCreated {
+        viewModel.filter.collectLatest {
+            filter.value = it
+        }
+    }
+
     viewModel.updateBusList(subwayState)
 
     Column {
@@ -48,6 +66,10 @@ fun InuScreen(viewModel: HomeViewModel, owner: LifecycleOwner, subwayState: Stri
                 .padding(horizontal = 20.dp)
                 .padding(top = 20.dp, bottom = 2.dp)
         ) {
+            BusStopFilter(
+                filterText = filter.value,
+                onFilterItemClicked = onFilterItemClicked,
+            )
             Spacer(modifier = Modifier.weight(1f, true))
             Text(text = "${updatedTime.value}기준")
         }
@@ -70,6 +92,49 @@ fun InuScreen(viewModel: HomeViewModel, owner: LifecycleOwner, subwayState: Stri
 }
 
 @Composable
+private fun BusStopFilter(
+    filterText: String,
+    onFilterItemClicked: (String) -> Unit,
+) {
+    val filterExpanded = remember {
+        mutableStateOf(false)
+    }
+    val onFilterExpansionChanged = { expanded: Boolean ->
+        println("확장 $expanded")
+        filterExpanded.value = expanded
+    }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.clickable {
+            onFilterExpansionChanged(true)
+        }
+    ) {
+        Text(text = filterText)
+        Icon(
+            painter = painterResource(id = R.drawable.down),
+            contentDescription = "bus stop filter dropdown menu",
+            tint = grayD9,
+            modifier = Modifier.width(12.dp)
+        )
+    }
+    DropdownMenu(expanded = filterExpanded.value, onDismissRequest = { onFilterExpansionChanged(false) }) {
+        DropdownMenuItem(onClick = {
+            onFilterItemClicked("전체")
+            onFilterExpansionChanged(false)
+        }) {
+            Text("전체")
+        }
+        DropdownMenuItem(onClick = {
+            onFilterItemClicked("공과대")
+            onFilterExpansionChanged(false)
+        }) {
+            Text("공과대")
+        }
+    }
+}
+
+@Composable
 private fun BusInfo(modifier: Modifier = Modifier, busArrivalInfo: BusArrivalInfo) {
     Column {
         Row(
@@ -80,8 +145,12 @@ private fun BusInfo(modifier: Modifier = Modifier, busArrivalInfo: BusArrivalInf
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
                     .padding(end = 11.dp)
-                    .background(color = Bus.getBusColorByBusNumber(busArrivalInfo.busNumber).color(),
-                        shape = RoundedCornerShape(size = 6.dp))
+                    .background(
+                        color = Bus
+                            .getBusColorByBusNumber(busArrivalInfo.busNumber)
+                            .color(),
+                        shape = RoundedCornerShape(size = 6.dp)
+                    )
                     .height(33.dp)
                     .width(52.dp),
             ) {
@@ -109,7 +178,7 @@ private fun BusInfo(modifier: Modifier = Modifier, busArrivalInfo: BusArrivalInf
 }
 
 private fun Int.toRestTimeFormat(): String {
-    if(this < 60) return "곧도착"
+    if (this < 60) return "곧도착"
 
     return "${this / 60}분 ${this % 60}초"
 }
@@ -132,7 +201,7 @@ private fun Chip(text: String) {
 }
 
 private fun String.color(): Color {
-    return when(this) {
+    return when (this) {
         "red" -> busRed
         "blue" -> busBlue
         "purple" -> busPurple
@@ -143,12 +212,6 @@ private fun String.color(): Color {
 
 @Preview(showBackground = true, widthDp = 360)
 @Composable
-fun ChipPreview() {
-//    BusInfo()
-}
-
-@Preview(showBackground = true, widthDp = 360, heightDp = 800)
-@Composable
-fun MainPreview() {
-//    BusInfo()
+fun WidgetPreview() {
+    BusStopFilter("전체", {})
 }
--- app/src/main/java/org/algosketch/inubus/presentation/ui/theme/Color.kt
+++ app/src/main/java/org/algosketch/inubus/presentation/ui/theme/Color.kt
@@ -8,6 +8,7 @@ val gray = Color(0xFFBAB6B6)
 val colorF9 = Color(0xFFFF9473)
 val grayDivider = Color(0x80D9D9D9)
 val gray66 = Color(0xFF666666)
+val grayD9 = Color(0xFFD9D9D9)
 
 val busRed = Color(0xFFBF3B3B)
 val busGreen = Color(0xFF66C654)
