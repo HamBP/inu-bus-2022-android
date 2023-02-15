4e887a5d8db06439e6cd94c2bd4e0b27ccb515f1
algosketch <sjy_9484@naver.com>
2023-02-15 21:23:19 +0900

    refactor: BusTabView 파일 분리

---
+++ app/src/main/java/org/algosketch/inubus/presentation/main/BusTab.kt
@@ -0,0 +1,54 @@
+package org.algosketch.inubus.presentation.main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import org.algosketch.inubus.presentation.ui.theme.gray
+import org.algosketch.inubus.presentation.ui.theme.primary
+
+@Composable
+fun BusTabView(onSelected: (MainDestination) -> Unit, currentScreen: MainDestination) {
+    Row(
+        Modifier
+            .selectableGroup()
+            .fillMaxWidth()
+    ) {
+        inuTabRowScreens.forEach { screen ->
+            val selected = currentScreen == screen
+
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .height(52.dp)
+                    .weight(1f, true)
+                    .selectable(
+                        selected = selected,
+                        onClick = { onSelected(screen) },
+                    )
+            ) {
+                Column {
+                    Spacer(modifier = Modifier.weight(1f, true))
+                    Text(
+                        text = screen.route,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                    )
+                    Spacer(modifier = Modifier.padding(bottom = 6.dp))
+                    Box(
+                        modifier = Modifier
+                            .background(color = if (selected) primary else gray)
+                            .fillMaxWidth()
+                            .height(4.dp)
+                    )
+                }
+            }
+        }
+    }
+}
