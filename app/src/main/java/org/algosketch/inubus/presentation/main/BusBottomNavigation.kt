5d382a73d5c72c985e99b896e9c0324242df03f1
algosketch <sjy_9484@naver.com>
2023-02-25 00:19:36 +0900

    feat: [네비게이션] bottomNavigation 클릭

--- app/src/main/java/org/algosketch/inubus/presentation/main/BusBottomNavigation.kt
+++ app/src/main/java/org/algosketch/inubus/presentation/main/BusBottomNavigation.kt
@@ -2,6 +2,7 @@ package org.algosketch.inubus.presentation.main
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
@@ -11,10 +12,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import org.algosketch.inubus.R
+import org.algosketch.inubus.presentation.navigation.NavDestination
 import org.algosketch.inubus.presentation.ui.theme.primary
 
 @Composable
-fun BusBottomNavigation() {
+fun BusBottomNavigation(destinations: List<NavDestination>, onSelected: (NavDestination) -> Unit, currentScreen: NavDestination) {
     Row(
         Modifier
             .selectableGroup()
@@ -25,7 +27,11 @@ fun BusBottomNavigation() {
             modifier = Modifier
                 .background(color = primary)
                 .fillMaxHeight()
-                .weight(1f),
+                .weight(1f)
+                .selectable(
+                    selected = destinations[0].route == currentScreen.route,
+                    onClick = { onSelected(destinations[0]) }
+                ),
             contentAlignment = Alignment.Center,
         ) {
             Icon(
@@ -39,7 +45,11 @@ fun BusBottomNavigation() {
             Modifier
                 .background(color = Color.White)
                 .fillMaxHeight()
-                .weight(1f),
+                .weight(1f)
+                .selectable(
+                    selected = destinations[1].route == currentScreen.route,
+                    onClick = { onSelected(destinations[1]) }
+                ),
             contentAlignment = Alignment.Center,
         ) {
             Icon(
