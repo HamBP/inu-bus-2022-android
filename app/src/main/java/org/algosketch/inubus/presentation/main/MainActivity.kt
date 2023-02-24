84546e5c127e0e0ed80b98b6936f537bd16e9e33
algosketch <sjy_9484@naver.com>
2023-02-24 21:09:55 +0900

    feat: 디테일 페이지 연결

--- app/src/main/java/org/algosketch/inubus/presentation/main/MainActivity.kt
+++ app/src/main/java/org/algosketch/inubus/presentation/main/MainActivity.kt
@@ -3,7 +3,8 @@ package org.algosketch.inubus.presentation.main
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material3.MaterialTheme
@@ -16,12 +17,15 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import org.algosketch.inubus.presentation.ui.detail.DetailScreen
 import org.algosketch.inubus.presentation.ui.home.ToSchoolViewModel
 import org.algosketch.inubus.presentation.ui.inu.InuScreen
 
 class MainActivity : AppCompatActivity() {
-    private val toSchoolViewModelFromInu: ToSchoolViewModel = ViewModelFactory.create(ToSchoolViewModel::class.java)
-    private val toSchoolViewModelFromBit: ToSchoolViewModel = ViewModelFactory.create(ToSchoolViewModel::class.java)
+    private val toSchoolViewModelFromInu: ToSchoolViewModel =
+        ViewModelFactory.create(ToSchoolViewModel::class.java)
+    private val toSchoolViewModelFromBit: ToSchoolViewModel =
+        ViewModelFactory.create(ToSchoolViewModel::class.java)
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -42,6 +46,24 @@ class MainActivity : AppCompatActivity() {
     fun InuBusApp() {
         val navController = rememberNavController()
         val currentBackStack by navController.currentBackStackEntryAsState()
+        NavHost(navController = navController, startDestination = "HOME") {
+            composable(route = "HOME") {
+                MainApp(parentNavHostController = navController)
+            }
+            composable(route = "DETAIL") {
+                DetailScreen(
+                    busNumber = "8",
+                    busStops = listOf("인천대입구", "인천대입구", "송도컨벤시아", "인천대입구"),
+                    lastStopName = "송도컨벤시아"
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun MainApp(parentNavHostController: NavHostController) {
+        val navController = rememberNavController()
+        val currentBackStack by navController.currentBackStackEntryAsState()
         val currentDestination = currentBackStack?.destination
 
         val currentScreen = toSchoolScreens.find { it.route == currentDestination?.route }
@@ -59,7 +81,8 @@ class MainActivity : AppCompatActivity() {
         ) { innerPadding ->
             BusNavHost(
                 navController = navController,
-                modifier = Modifier.padding(innerPadding)
+                modifier = Modifier.padding(innerPadding),
+                parentNavController = parentNavHostController,
             )
         }
     }
@@ -67,7 +90,8 @@ class MainActivity : AppCompatActivity() {
     @Composable
     fun BusNavHost(
         navController: NavHostController,
-        modifier: Modifier = Modifier
+        modifier: Modifier = Modifier,
+        parentNavController: NavHostController,
     ) {
         NavHost(
             navController = navController,
@@ -78,14 +102,16 @@ class MainActivity : AppCompatActivity() {
                 InuScreen(
                     owner = this@MainActivity,
                     viewModel = toSchoolViewModelFromInu,
-                    subwayState = "인천대입구"
+                    subwayState = "인천대입구",
+                    navController = parentNavController
                 )
             }
             composable(route = BIT.route) {
                 InuScreen(
                     owner = this@MainActivity,
                     viewModel = toSchoolViewModelFromBit,
-                    subwayState = "지식정보단지"
+                    subwayState = "지식정보단지",
+                    navController = parentNavController
                 )
             }
         }
