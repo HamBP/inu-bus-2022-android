12588a121959a102836c02bfe24cdafdea316c6b
algosketch <sjy_9484@naver.com>
2023-03-04 18:57:03 +0900

    refactor: 기존 디테일 fragment 삭제

--- app/src/main/java/org/algosketch/inubus/presentation/ui/detail/DetailViewModel.kt
+++ app/src/main/java/org/algosketch/inubus/presentation/ui/detail/DetailViewModel.kt
@@ -1,22 +1,6 @@
 package org.algosketch.inubus.presentation.ui.detail
 
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import org.algosketch.inubus.common.base.BaseViewModel
-import org.algosketch.inubus.common.util.SingleLiveEvent
 
 class DetailViewModel : BaseViewModel() {
-    val busNumber = MutableStateFlow("")
-//    val busNumber = _busNumber.asStateFlow()
-
-    val restTime = MutableStateFlow("")
-    val where = MutableStateFlow("")
-    val exit = MutableStateFlow("")
-    val distance = MutableStateFlow("")
-    val imageId = MutableStateFlow<Int?>(null)
-    val undoEvent = SingleLiveEvent<Any>()
-
-    fun undo() {
-        undoEvent.call()
-    }
 }
