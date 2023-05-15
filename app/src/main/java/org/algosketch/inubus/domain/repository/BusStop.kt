97bcf0ce15df00a90242204e4cc134670433e4ae
algosketch <sjy_9484@naver.com>
2023-05-15 22:37:53 +0900

    Feat: 버스 정류장을 관리할 enum 추가

---
+++ app/src/main/java/org/algosketch/inubus/domain/repository/BusStop.kt
@@ -0,0 +1,10 @@
+package org.algosketch.inubus.domain.repository
+
+enum class BusStop {
+    INU_STATION_EXIT_1,
+    INU_STATION_EXIT_2,
+    BIT_3,
+    BIT_4,
+    MAIN_GATE_OF_INU,
+    COLLEGE_OF_ENGINEERING,
+}
